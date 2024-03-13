754de0eb2fab9332e750d46955e848b855a7d906
Rene Pringi <rpringi@gmail.com>
2024-03-13 14:34:32 +0200

    update

--- src/workshop_rene/coffee-script.ts
+++ src/workshop_rene/coffee-script.ts
@@ -3,7 +3,7 @@ let mymilkAmount: number=50;
 let mycoffeName: string="Cappuchino";
 //console.log(`My name is:${MY_NAME}! Your coffe is ${mycoffeName} and it has ${mymilkAmount} ml of milk in it`);
 
-if(isTooMuchMilk(40)){
+if(isTooMuchMilk1(40)){
     console.log('result1');
 }else if(mycoffeName==="Test"){
     console.log('result2');
@@ -11,6 +11,6 @@ if(isTooMuchMilk(40)){
     console.log("result3");
 }
 
-function isTooMuchMilk(amount:number):boolean{
+function isTooMuchMilk1(amount:number):boolean{
     return mymilkAmount>amount;
 }
